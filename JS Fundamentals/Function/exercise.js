659a1242b0c673f6bc961778662d26035971f278
veysel-ruskov <nabacarry@abv.bg>
2021-11-03 14:54:58 +0200

    Add file

--- JS Fundamentals/Function/exercise.js
+++ JS Fundamentals/Function/exercise.js
@@ -80,4 +80,132 @@ for (let i = 0; i < input.length; i += 2) {
 
     str += '</quiz>';
     return str;
-}console.log(templateFormat(['question', 'answer','Dry ice is a frozen form of which gas?', 'Carbon Dioxide']));
+}//console.log(templateFormat(['question', 'answer','Dry ice is a frozen form of which gas?', 'Carbon Dioxide']));
+
+//4
+function cookingByNumbers(input) {
+    let num = Number(input[0]);
+    for (let i = 1; i < input.length; i++) {
+        let func = input[i];
+        activities(func);
+
+    }
+    function activities(func) {
+
+        if (func == 'dice') {
+            num = Math.sqrt(num);
+        }
+        if (func == 'chop') {
+            num /= 2;
+        }
+        if (func == 'spice') {
+            ++num;
+        }
+        if (func == 'bake') {
+            num *= 3;
+        }
+        if (func == 'fillet') {
+            num *= 0.8;
+        }
+        console.log(num);
+    }
+}
+let input = [32, 'chop', 'chop', 'chop', 'chop', 'chop'];
+//cookingByNumbers(input);
+
+//5
+function modifyAverage(input) {
+    let number = String(input[0]);
+    let sum = 0;
+    while (true) {
+        for (let i = 0; i < number.length; i++) {
+            let element = Number(number[i]);
+            sum += element;
+        }
+        if(sum / number.length > 5){
+            console.log(number);
+            break;
+        }else{
+            number += 9;
+            sum = 0;
+        }
+    }
+}//modifyAverage([101]);
+
+//6
+function validityChecker(input) {
+    let x1 = Number(input[0]);
+    let y1 = Number(input[1]);
+    let x2 = Number(input[2]);
+    let y2 = Number(input[3]);
+
+    let distanceTo01 = Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2));
+    if (distanceTo01 === parseInt(distanceTo01)) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    }
+    else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+
+    let distanceTo02 = Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2));
+    if (distanceTo02 === parseInt(distanceTo02)) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    }
+    else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+
+    let distanceBetweenPoints = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    if (distanceBetweenPoints === parseInt(distanceBetweenPoints)) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    }
+    else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}//validityChecker([3, 0, 0, 4]);
+
+//7
+function shapeCrystals(arr) {
+    let targetSize = arr[0];
+
+    let cut = num => num / 4;
+    let lap = num => num * 0.8;
+    let grind = num => num - 20;
+    let etch = num => num - 2;
+    let transportAndWashing = num => {
+        console.log(`Transporting and washing`);
+        return Math.floor(num);
+    }
+    let xray = num => {
+        console.log(`X-ray x1`);
+       return ++num;
+    };
+
+    for (let i = 0; i < arr.length; i++) {
+        let microns = arr[i];
+        microns = executeOperation(targetSize, microns, 'Cut', cut);
+        microns = executeOperation(targetSize, microns, 'Lap', lap);
+        microns = executeOperation(targetSize, microns, 'Grind', grind);
+        microns = executeOperation(targetSize, microns, 'Etch', etch);
+
+        if(microns + 1 == targetSize) {
+            microns = xray(microns);
+        }
+        console.log(`Finished crystal ${microns} microns`);
+    }
+    function executeOperation(targetSize, size, operationName, operation) {
+        let counter = 0;
+        size = operation(size);
+        while (operation(size) >= targetSize || targetSize - operation(size) === 1) {
+            size = operation(size);
+            counter++;
+        } 
+        if(counter > 0) {
+            console.log(`${operationName} x${counter}`);
+            size = transportAndWashing(size);
+        }
+        return size;
+    }
+}
+let crystal = [1375, 50000];
+shapeCrystals(crystal);
